32030287f16e0bb19b7a63958141c20e42937a7d
Aditya <ricky.pathak902@gmail.com>
2025-02-10 12:52:51 +0530

    setting up the project

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Job {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+  salary: string;
+  rating: number;
+  reviews: number;
+  tags: string[];
+  logo: string;
+}
